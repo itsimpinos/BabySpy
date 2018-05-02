d284af12d50709fcf124ef9c6202606d541a3a10
itsimpinos <38903651+itsimpinos@users.noreply.github.com>
2018-05-02 13:55:47 +0300

    Create pir.py

---
+++ code/pir.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import datetime
+import time
+
+
+pir=21
+try:
+    while True:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pir,GPIO.IN)
+        if GPIO.input(pir):
+            print('Kίνηση...:',datetime.datetime.now().strftime('%d-%m-%y %H:%M:%S'))
+            time.sleep(1)
+                        
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('Καλή Συνέχεια')
+    
